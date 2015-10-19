29df249ce86d680eaff9dad78901fed2181ba2f8
jjakim <jjjakim@gmail.com>
2015-10-18 17:08:58 -0700

    This is a first commit of 'single'.
    Let's try again for a first commit.

---
+++ dualtul.pl
@@ -0,0 +1,50 @@
+#!/usr/bin/perl -w
+
+######################################
+#
+# v0.0.1 dualtul.pl
+#
+#read and store dual weights as text file
+#
+####################################
+
+use strict;
+use warnings;
+
+#TODO: add log4perl
+
+my $raw0 =0;
+my $raw1 =0;
+my $datestr;
+my $weight0;
+my $weight1;
+my $cpuTemp;
+
+
+# read the scales
+
+$weight0 = `sudo /home/pi/hx711/hx711-0 1`;
+$weight0 = $weight0/9200;
+
+$weight1 = `sudo /home/pi/hx711/hx711-1 1`;
+$weight1 = $weight1/7300;
+
+$cpuTemp = `vcgencmd measure_temp`;
+$cpuTemp =~ m/\d+\.\d/;
+$cpuTemp = $&;
+
+# $datestr = localtime();
+$datestr = time();
+
+
+# write the file
+# my $csvlog = '/media/usbstick/dual-091215.csv';
+my $csvlog = '/media/usbstick/www/data0.csv';
+
+
+open(my $fh, '>>', $csvlog );
+print $fh $datestr,", ", $weight0, ", ", $weight1, ", ", $cpuTemp, "\n";
+close $fh; 
+
+
+
